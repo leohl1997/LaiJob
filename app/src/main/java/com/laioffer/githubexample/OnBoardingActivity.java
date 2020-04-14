98aa0a25d223cbccdc022c3f6b59470e6e1e5dca
leo <leih5900@gmail.com>
2020-04-14 03:27:03 -0500

    Create OnBoardingActivity.java

---
+++ app/src/main/java/com/laioffer/githubexample/OnBoardingActivity.java
@@ -0,0 +1,14 @@
+package com.laioffer.githubexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
