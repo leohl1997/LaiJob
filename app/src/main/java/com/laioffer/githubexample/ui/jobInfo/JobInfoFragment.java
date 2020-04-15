6963f4a9896657e3476f13d0ccc42f842973f7b7
leo <leih5900@gmail.com>
2020-04-14 22:00:55 -0500

    Update JobInfoFragment.java

--- app/src/main/java/com/laioffer/githubexample/ui/jobInfo/JobInfoFragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/jobInfo/JobInfoFragment.java
@@ -1,4 +1,38 @@
 package com.laioffer.githubexample.ui.jobInfo;
 
-public class JobInfoFragment {
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.githubexample.R;
+
+public class JobInfoFragment extends Fragment {
+
+    private JobInfoViewModel mViewModel;
+
+    public static JobInfoFragment newInstance() {
+        return new JobInfoFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.job_info_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(JobInfoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
 }
