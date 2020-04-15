842d0be29638f27c0b37a021b91de2601de2d81b
leo <leih5900@gmail.com>
2020-04-14 22:00:46 -0500

    Update HomeMapFragment.java

--- app/src/main/java/com/laioffer/githubexample/ui/HomeMap/HomeMapFragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/HomeMap/HomeMapFragment.java
@@ -1,4 +1,38 @@
 package com.laioffer.githubexample.ui.HomeMap;
 
-public class HomeMapFragment {
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.githubexample.R;
+
+public class HomeMapFragment extends Fragment {
+
+    private HomeMapViewModel mViewModel;
+
+    public static HomeMapFragment newInstance() {
+        return new HomeMapFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_map_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(HomeMapViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
 }
