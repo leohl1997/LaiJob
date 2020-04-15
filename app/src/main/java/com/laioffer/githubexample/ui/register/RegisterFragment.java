cc780ae7b9476ce83f54a88fdd6d61326b4bf2ed
leo <leih5900@gmail.com>
2020-04-14 22:01:11 -0500

    Update RegisterFragment.java

--- app/src/main/java/com/laioffer/githubexample/ui/register/RegisterFragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/register/RegisterFragment.java
@@ -1,4 +1,38 @@
 package com.laioffer.githubexample.ui.register;
 
-public class RegisterFragment {
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.githubexample.R;
+
+public class RegisterFragment extends Fragment {
+
+    private RegisterViewModel mViewModel;
+
+    public static RegisterFragment newInstance() {
+        return new RegisterFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.register_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(RegisterViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
 }
