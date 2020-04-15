287eb3e2ae4c7f4fe0d13c348432955bf874400b
leo <leih5900@gmail.com>
2020-04-14 21:59:03 -0500

    Update FavoriteJobfragment.java

--- app/src/main/java/com/laioffer/githubexample/ui/favorite/FavoriteJobfragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/favorite/FavoriteJobfragment.java
@@ -1,4 +1,38 @@
 package com.laioffer.githubexample.ui.favorite;
 
-public class FavoriteJobfragment {
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.githubexample.R;
+
+public class FavoriteJobFragment extends Fragment {
+
+    private FavoriteJobViewModel mViewModel;
+
+    public static FavoriteJobFragment newInstance() {
+        return new FavoriteJobFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.favorite_job_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(FavoriteJobViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
 }
