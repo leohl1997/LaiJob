eb90cffd196b82f8734cb3288cda23946f32532d
leo <leih5900@gmail.com>
2020-04-14 22:01:01 -0500

    Update LoginFragment.java

--- app/src/main/java/com/laioffer/githubexample/ui/login/LoginFragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/login/LoginFragment.java
@@ -1,4 +1,38 @@
 package com.laioffer.githubexample.ui.login;
 
-public class LoginFragment {
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.githubexample.R;
+
+public class LoginFragment extends Fragment {
+
+    private LoginViewModel mViewModel;
+
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.login_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
 }
