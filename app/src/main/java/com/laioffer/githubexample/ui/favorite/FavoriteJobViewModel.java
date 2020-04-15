80fa70980d985f56c193d505914e1be08190198e
leo <leih5900@gmail.com>
2020-04-14 21:59:06 -0500

    Create FavoriteJobViewModel.java

---
+++ app/src/main/java/com/laioffer/githubexample/ui/favorite/FavoriteJobViewModel.java
@@ -0,0 +1,7 @@
+package com.laioffer.githubexample.ui.favorite;
+
+import androidx.lifecycle.ViewModel;
+
+public class FavoriteJobViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
