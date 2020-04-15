debb048d1a994c91317636a6e70e29e0dd030f3a
leo <leih5900@gmail.com>
2020-04-14 22:01:17 -0500

    Update SearchFragment.java

--- app/src/main/java/com/laioffer/githubexample/ui/search/SearchFragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/search/SearchFragment.java
@@ -1,4 +1,38 @@
 package com.laioffer.githubexample.ui.search;
 
-public class SearchFragment {
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.githubexample.R;
+
+public class SearchFragment extends Fragment {
+
+    private SearchViewModel mViewModel;
+
+    public static SearchFragment newInstance() {
+        return new SearchFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.search_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
 }
