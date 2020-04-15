0e97d5d982964a95c5f6665bdd67a5a40a0bb595
leo <leih5900@gmail.com>
2020-04-14 22:01:30 -0500

    Update UserInfoFragment.java

--- app/src/main/java/com/laioffer/githubexample/ui/userInfo/UserInfoFragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/userInfo/UserInfoFragment.java
@@ -1,4 +1,38 @@
 package com.laioffer.githubexample.ui.userInfo;
 
-public class UserInfoFragment {
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.githubexample.R;
+
+public class UserInfoFragment extends Fragment {
+
+    private UserInfoViewModel mViewModel;
+
+    public static UserInfoFragment newInstance() {
+        return new UserInfoFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.user_info_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(UserInfoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
 }
