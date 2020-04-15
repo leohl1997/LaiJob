f7fecaea47a49d5d5de4bf3ba519b1f4c67d438b
leo <leih5900@gmail.com>
2020-04-14 21:59:10 -0500

    Update HomeListFragment.java

--- app/src/main/java/com/laioffer/githubexample/ui/HomeList/HomeListFragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/HomeList/HomeListFragment.java
@@ -1,4 +1,37 @@
 package com.laioffer.githubexample.ui.HomeList;
 
-public class HomeListFragment {
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.githubexample.R;
+
+public class HomeListFragment extends Fragment {
+
+    private HomeListViewModel mViewModel;
+
+    public static HomeListFragment newInstance() {
+        return new HomeListFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_list_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(HomeListViewModel.class);
+        // TODO: Use the ViewModel
+    }
 }
