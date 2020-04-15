635b99c0334eb3ce007b5df45c3343caf4f54cd8
leo <leih5900@gmail.com>
2020-04-14 22:01:14 -0500

    Create RegisterViewModel.java

---
+++ app/src/main/java/com/laioffer/githubexample/ui/register/RegisterViewModel.java
@@ -0,0 +1,7 @@
+package com.laioffer.githubexample.ui.register;
+
+import androidx.lifecycle.ViewModel;
+
+public class RegisterViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
