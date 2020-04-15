040404596b47e8ba7eb14b52be98b543a38dc8ca
leo <leih5900@gmail.com>
2020-04-15 15:45:02 -0500

    Update MainActivity.java

--- app/src/main/java/com/laioffer/githubexample/MainActivity.java
+++ app/src/main/java/com/laioffer/githubexample/MainActivity.java
@@ -7,20 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.laioffer.githubexample.ui.HomeList.HomeListFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Button button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
+
 
     // add Fragment to the activity
     //getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, MainFragment.newInstance()).commit();
