4793ea1f68c09e2c4252c67236ddff7044d467d5
leo <leih5900@gmail.com>
2020-04-17 03:59:21 -0500

    Update RegisterFragment.java

--- app/src/main/java/com/laioffer/githubexample/ui/register/RegisterFragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/register/RegisterFragment.java
@@ -1,7 +1,9 @@
 package com.laioffer.githubexample.ui.register;
 
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,21 +13,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.laioffer.githubexample.R;
+import com.laioffer.githubexample.base.BaseFragment;
+import com.laioffer.githubexample.base.BaseRepository;
+import com.laioffer.githubexample.base.BaseViewModel;
+import com.laioffer.githubexample.ui.HomeList.HomeListFragment;
+import com.laioffer.githubexample.ui.NavigationManager;
+import com.laioffer.githubexample.ui.login.LoginFragment;
 
-public class RegisterFragment extends Fragment {
+public class RegisterFragment extends BaseFragment {
 
     private RegisterViewModel mViewModel;
 
     public static RegisterFragment newInstance() {
         return new RegisterFragment();
     }
+    NavigationManager navigationManager;
 
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        navigationManager = (NavigationManager) context;
+    }
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.register_fragment, container, false);
+        View view = inflater.inflate(R.layout.login_fragment, container, false);
+        Button button1 = view.findViewById(R.id.first_button);
+        button1.setText("Submit");
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //navigationManager.navigateTo(new HomeListFragment());
+            }
+        });
+        Button button2 = view.findViewById(R.id.second_button);
+        button2.setText("Back Login");
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigationManager.navigateTo(new LoginFragment());
+            }
+        });
+        return view;
     }
 
     @Override
@@ -35,4 +66,18 @@ public class RegisterFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    @Override
+    protected BaseViewModel getViewModel() {
+        return null;
+    }
+
+    @Override
+    protected ViewModelProvider.Factory getFactory() {
+        return null;
+    }
+
+    @Override
+    protected BaseRepository getRepository() {
+        return null;
+    }
 }
