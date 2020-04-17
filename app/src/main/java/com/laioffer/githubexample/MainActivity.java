0fd7f56274e8a842b2446861d42380301d7abeb6
leo <leih5900@gmail.com>
2020-04-17 03:59:00 -0500

    Update MainActivity.java

--- app/src/main/java/com/laioffer/githubexample/MainActivity.java
+++ app/src/main/java/com/laioffer/githubexample/MainActivity.java
@@ -1,6 +1,7 @@
 package com.laioffer.githubexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,13 +9,26 @@ import android.view.View;
 import android.widget.Button;
 
 import com.laioffer.githubexample.ui.HomeList.HomeListFragment;
+import com.laioffer.githubexample.ui.NavigationManager;
+import com.laioffer.githubexample.ui.login.LoginFragment;
+import com.laioffer.githubexample.ui.search.SearchFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationManager {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        navigateTo(new LoginFragment());
+    }
+
+    @Override
+    public void navigateTo(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.first_fragment, fragment, null)
+                .addToBackStack(null)
+                .commit();
     }
 
 
