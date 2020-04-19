aeff6470565864aea2f347912f2b7bb25e5b3a1b
leo <leih5900@gmail.com>
2020-04-19 18:33:52 -0500

    Update MainActivity.java

--- app/src/main/java/com/laioffer/githubexample/MainActivity.java
+++ app/src/main/java/com/laioffer/githubexample/MainActivity.java
@@ -1,20 +1,31 @@
 package com.laioffer.githubexample;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.laioffer.githubexample.ui.HomeList.HomeListFragment;
 import com.laioffer.githubexample.ui.NavigationManager;
 import com.laioffer.githubexample.ui.login.LoginFragment;
 import com.laioffer.githubexample.ui.search.SearchFragment;
+import com.laioffer.githubexample.util.Config;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements NavigationManager {
-
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +41,4 @@ public class MainActivity extends AppCompatActivity implements NavigationManager
                 .addToBackStack(null)
                 .commit();
     }
-
-
-    // add Fragment to the activity
-    //getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, MainFragment.newInstance()).commit();
 }
