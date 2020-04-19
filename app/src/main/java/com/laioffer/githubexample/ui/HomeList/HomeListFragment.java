57897655e27436fb7d27bb448adb1a365ea66769
leo <leih5900@gmail.com>
2020-04-19 18:34:34 -0500

    Update HomeListFragment.java
    向HomeListFragment添加菜单选项，并设置菜单跳转

--- app/src/main/java/com/laioffer/githubexample/ui/HomeList/HomeListFragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/HomeList/HomeListFragment.java
@@ -1,5 +1,10 @@
 package com.laioffer.githubexample.ui.HomeList;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -11,24 +16,31 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.laioffer.githubexample.R;
 import com.laioffer.githubexample.base.BaseFragment;
 import com.laioffer.githubexample.base.BaseRepository;
 import com.laioffer.githubexample.base.BaseViewModel;
 import com.laioffer.githubexample.ui.HomeMap.HomeMapFragment;
 import com.laioffer.githubexample.ui.NavigationManager;
+import com.laioffer.githubexample.ui.favorite.FavoriteJobFragment;
 import com.laioffer.githubexample.ui.jobInfo.JobInfoFragment;
 import com.laioffer.githubexample.ui.search.SearchFragment;
 import com.laioffer.githubexample.ui.userInfo.UserInfoFragment;
+import com.laioffer.githubexample.util.Config;
 
 public class HomeListFragment extends BaseFragment {
     private HomeListViewModel mViewModel;
-    NavigationManager navigationManager;
+    private NavigationManager navigationManager;
+    private DrawerLayout drawerLayout;
+    private AppCompatActivity mactivity;
 
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -42,7 +54,92 @@ public class HomeListFragment extends BaseFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         View view = inflater.inflate(R.layout.home_list_fragment, container, false);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+
+        mactivity = (AppCompatActivity) getActivity();
+        assert mactivity != null;
+        mactivity.setSupportActionBar(toolbar);
+        ActionBar actionbar = mactivity.getSupportActionBar();
+        assert actionbar != null;
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.baseline_home_black_18dp);
+
+
+        drawerLayout = view.findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = view.findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        if (menuItem.getItemId() == R.id.drawer_logout) {
+                            Config.username = null;
+                            mactivity.finish();
+                        }
+                        if (menuItem.getItemId() == R.id.user_info) {
+                            Config.username = null;
+                            navigationManager.navigateTo(new UserInfoFragment());
+                        }
+                        if (menuItem.getItemId() == R.id.favorite) {
+                            Config.username = null;
+                            navigationManager.navigateTo(new FavoriteJobFragment());
+                        }
+                        if (menuItem.getItemId() == R.id.search) {
+                            Config.username = null;
+                            navigationManager.navigateTo(new SearchFragment());
+                        }
+                        return true;
+                    }
+                });
+
+        drawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+                    }
+
+                    @Override
+                    public void onDrawerOpened(@NonNull View drawerView) {
+                        final TextView user_textview = (TextView) drawerView.findViewById(R.id.user_name);
+                        final TextView location_textview = (TextView) drawerView.findViewById(R.id.user_location);
+
+//                        // Respond when the drawer is opened
+//                        mLocationTracker.getLocation();
+//                        final double longitude = mLocationTracker.getLongitude();
+//                        final double latitude = mLocationTracker.getLatitude();
+
+                        if (Config.username == null) {
+                            user_textview.setText("");
+                            location_textview.setText("");
+                        } else {
+                            user_textview.setText(Config.username);
+//                            location_textview.setText("Lat=" + new DecimalFormat(".##").
+//                                    format(latitude) + ",Lon=" + new DecimalFormat(".##").
+//                                    format(longitude));
+                        }
+                    }
+
+                    @Override
+                    public void onDrawerClosed(@NonNull View drawerView) {
+
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+
+                    }
+                }
+        );
         Button button1 = view.findViewById(R.id.search);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +171,7 @@ public class HomeListFragment extends BaseFragment {
         return view;
     }
 
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -81,7 +179,15 @@ public class HomeListFragment extends BaseFragment {
         // TODO: Use the ViewModel
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected BaseViewModel getViewModel() {
