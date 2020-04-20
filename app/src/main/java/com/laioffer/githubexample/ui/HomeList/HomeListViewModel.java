f508c81ff7c395271e12dd5437294eed8f0521cb
leo <leih5900@gmail.com>
2020-04-20 05:20:17 -0500

    Update HomeListViewModel.java

--- app/src/main/java/com/laioffer/githubexample/ui/HomeList/HomeListViewModel.java
+++ app/src/main/java/com/laioffer/githubexample/ui/HomeList/HomeListViewModel.java
@@ -1,7 +1,20 @@
 package com.laioffer.githubexample.ui.HomeList;
 
-import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.MutableLiveData;
 
-public class HomeListViewModel extends ViewModel {
+import com.laioffer.githubexample.base.BaseViewModel;
+
+public class HomeListViewModel extends BaseViewModel<HomeListRepository> {
+    protected HomeListViewModel(HomeListRepository baseRepository) {
+        super(baseRepository);
+    }
+    private MutableLiveData<String> mUserName;
+    private String getUserName(){
+        if(mUserName == null){
+            mUserName = new MutableLiveData<String>();
+        }
+        return "user";
+    }
+
     // TODO: Implement the ViewModel
 }
