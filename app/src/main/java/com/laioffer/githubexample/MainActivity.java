d72623aad711b7e77c81d767377786110456adc0
Rujun Yao <rujunyao@Rujuns-MacBook-Pro-2.local>
2020-04-24 22:50:56 -0700

    minor changes

--- app/src/main/java/com/laioffer/githubexample/MainActivity.java
+++ app/src/main/java/com/laioffer/githubexample/MainActivity.java
@@ -1,38 +1,21 @@
 package com.laioffer.githubexample;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
 import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.crash.FirebaseCrash;
-import com.laioffer.githubexample.ui.HomeList.HomeListFragment;
 import com.laioffer.githubexample.ui.NavigationManager;
-import com.laioffer.githubexample.ui.login.LoginFragment;
-import com.laioffer.githubexample.ui.map.MapFragment;
-import com.laioffer.githubexample.ui.search.SearchFragment;
 import com.laioffer.githubexample.util.Config;
 import com.laioffer.githubexample.util.Utils;
-
-import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements NavigationManager, LocationListener {
     private DrawerLayout drawerLayout;
@@ -42,6 +25,11 @@ public class MainActivity extends AppCompatActivity implements NavigationManager
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        getLocation();
+
         navigateTo(new OnBoardingSplashFragment());
 //        navigateTo(new OnBoardingBaseFragment());
     }
@@ -68,8 +56,6 @@ public class MainActivity extends AppCompatActivity implements NavigationManager
 
     void getLocation() {
         try {
-            ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION}, 1);
             LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
         }
@@ -82,8 +68,6 @@ public class MainActivity extends AppCompatActivity implements NavigationManager
     public void onLocationChanged(Location location) {
         Config.lat = location.getLatitude();
         Config.lon = location.getLongitude();
-        String msg = "Latitude: " + Config.lat + ", Longitude: " + Config.lon;
-        Utils.constructToast(MainActivity.this, msg).show();
     }
 
     @Override
