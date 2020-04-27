10f33b65bd2de407261756605f88835a3ff1b3d2
leo <leih5900@gmail.com>
2020-04-26 21:52:42 -0500

    Update NavigationManager.java

--- app/src/main/java/com/laioffer/githubexample/ui/NavigationManager.java
+++ app/src/main/java/com/laioffer/githubexample/ui/NavigationManager.java
@@ -6,4 +6,6 @@ public interface NavigationManager {
 
     void navigateTo(Fragment fragment);
     void navigateToWithAnimation(Fragment fragment);
+    void navigateWithFragmentDestroy(Fragment target, Fragment current);
+    void goBack();
 }
