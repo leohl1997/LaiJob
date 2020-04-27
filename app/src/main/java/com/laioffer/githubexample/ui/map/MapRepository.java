05acb600a988fe34d16126c921f233e226aeeae0
leo <leih5900@gmail.com>
2020-04-27 02:19:49 -0500

    Update MapRepository.java

--- app/src/main/java/com/laioffer/githubexample/ui/map/MapRepository.java
+++ app/src/main/java/com/laioffer/githubexample/ui/map/MapRepository.java
@@ -5,7 +5,7 @@ import androidx.lifecycle.MutableLiveData;
 import com.laioffer.githubexample.base.BaseRepository;
 import com.laioffer.githubexample.remote.response.Job;
 import com.laioffer.githubexample.remote.response.RemoteResponse;
-import com.laioffer.githubexample.util.config;
+import com.laioffer.githubexample.util.Config;
 
 import java.util.List;
 
@@ -17,11 +17,7 @@ import retrofit2.internal.EverythingIsNonNull;
 public class MapRepository extends BaseRepository {
     MutableLiveData<List<Job>> search(String keyword) {
         MutableLiveData<List<Job>> listMutableLiveData = new MutableLiveData<>();
-<<<<<<< HEAD
-        Call<RemoteResponse<List<Job>>> call = apiService.search(config.lat, config.lon);
-=======
         Call<RemoteResponse<List<Job>>> call = apiService.search(Config.latitude, Config.longitude);
->>>>>>> bc0c1af1071fcf9a78b627c46fc9c842d2395ee1
         call.enqueue(new Callback<RemoteResponse<List<Job>>>() {
             @EverythingIsNonNull
             @Override
