c12c7a66d9a153066b6053550c963b07515d34a1
leo <leih5900@gmail.com>
2020-04-27 02:19:38 -0500

    Update EditWorkViewModel.java

--- app/src/main/java/com/laioffer/githubexample/ui/editWork/EditWorkViewModel.java
+++ app/src/main/java/com/laioffer/githubexample/ui/editWork/EditWorkViewModel.java
@@ -2,8 +2,8 @@ package com.laioffer.githubexample.ui.editWork;
 
 import com.laioffer.githubexample.base.BaseViewModel;
 
-public class EditWorkViewModel extends BaseViewModel<EditWorkRepository> {
-    protected EditWorkViewModel(EditWorkRepository baseRepository) {
+public class editWorkViewModel extends BaseViewModel<EditWorkRepository> {
+    protected editWorkViewModel(EditWorkRepository baseRepository) {
         super(baseRepository);
     }
     // TODO: Implement the ViewModel
