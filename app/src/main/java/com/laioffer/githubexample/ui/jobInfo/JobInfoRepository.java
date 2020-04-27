cc30ca50f99507e54c7ad333b427c64bb8c37e08
leo <leih5900@gmail.com>
2020-04-26 21:52:20 -0500

    Create JobInfoRepository.java

---
+++ app/src/main/java/com/laioffer/githubexample/ui/jobInfo/JobInfoRepository.java
@@ -0,0 +1,90 @@
+package com.laioffer.githubexample.ui.jobInfo;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.laioffer.githubexample.base.BaseRepository;
+import com.laioffer.githubexample.remote.response.Job;
+import com.laioffer.githubexample.remote.response.RemoteResponse;
+import com.laioffer.githubexample.ui.comment.CommentEvent;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.internal.EverythingIsNonNull;
+
+public class JobInfoRepository extends BaseRepository {
+    public MutableLiveData<List<CommentEvent>> getComment(String jobId) {
+        MutableLiveData<List<CommentEvent>> responseLiveData = new MutableLiveData<>();
+        Call<RemoteResponse<List<CommentEvent>>> call = apiService.getComment(jobId);
+        call.enqueue(new Callback<RemoteResponse<List<CommentEvent>>>() {
+            @EverythingIsNonNull
+            @Override
+            public void onResponse(Call<RemoteResponse<List<CommentEvent>>> call, Response<RemoteResponse<List<CommentEvent>>> response) {
+                if (response.code() == 200) {
+                    responseLiveData.postValue(response.body().response);
+                } else {
+                    responseLiveData.postValue(null);
+                }
+            }
+
+            @EverythingIsNonNull
+            @Override
+            public void onFailure(Call<RemoteResponse<List<CommentEvent>>> call, Throwable t) {
+                responseLiveData.postValue(null);
+            }
+        });
+        return responseLiveData;
+    }
+
+    public MutableLiveData<String> save(SaveEvent saveEvent) {
+        return saveEvent.job.favorite ? unfavorite(saveEvent) : favorite(saveEvent);
+    }
+
+    private MutableLiveData<String> favorite(SaveEvent saveEvent) {
+        MutableLiveData<String>  responseLiveDate = new MutableLiveData<>();
+        Call<RemoteResponse<SaveEvent>> call = apiService.favorite(saveEvent);
+        call.enqueue(new Callback<RemoteResponse<SaveEvent>>() {
+            @EverythingIsNonNull
+            @Override
+            public void onResponse(Call<RemoteResponse<SaveEvent>> call, Response<RemoteResponse<SaveEvent>> response) {
+                if (response.code() == 200) {
+                    responseLiveDate.postValue("Save Success!");
+                } else {
+                    responseLiveDate.postValue("Error");
+                }
+            }
+
+            @EverythingIsNonNull
+            @Override
+            public void onFailure(Call<RemoteResponse<SaveEvent>> call, Throwable t) {
+                responseLiveDate.postValue("Error");
+            }
+        });
+        return responseLiveDate;
+    }
+
+    private MutableLiveData<String> unfavorite(SaveEvent saveEvent) {
+        MutableLiveData<String>  responseLiveDate = new MutableLiveData<>();
+        Call<RemoteResponse<SaveEvent>> call = apiService.unfavorite(saveEvent);
+        call.enqueue(new Callback<RemoteResponse<SaveEvent>>() {
+            @EverythingIsNonNull
+            @Override
+            public void onResponse(Call<RemoteResponse<SaveEvent>> call, Response<RemoteResponse<SaveEvent>> response) {
+                if (response.code() == 200) {
+                    responseLiveDate.postValue("Unsave Success!");
+                } else {
+                    responseLiveDate.postValue("Error");
+                }
+            }
+
+            @EverythingIsNonNull
+            @Override
+            public void onFailure(Call<RemoteResponse<SaveEvent>> call, Throwable t) {
+                responseLiveDate.postValue("Error");
+            }
+        });
+        return responseLiveDate;
+    }
+}
