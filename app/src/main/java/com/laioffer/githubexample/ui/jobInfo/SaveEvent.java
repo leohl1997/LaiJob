ba0d7005680536f7007a58ef5f89a00c3668b320
leo <leih5900@gmail.com>
2020-04-26 21:52:24 -0500

    Create SaveEvent.java

---
+++ app/src/main/java/com/laioffer/githubexample/ui/jobInfo/SaveEvent.java
@@ -0,0 +1,12 @@
+package com.laioffer.githubexample.ui.jobInfo;
+
+import com.google.gson.annotations.SerializedName;
+import com.laioffer.githubexample.remote.response.Job;
+
+public class SaveEvent {
+    @SerializedName("user_id")
+    public String userId;
+    @SerializedName("favorite")
+    public Job job;
+
+}
