070206404622b61f505d058cd0ca042d0495ec74
leo <leih5900@gmail.com>
2020-04-26 21:43:58 -0500

    Create EditWorkFragment.java

---
+++ app/src/main/java/com/laioffer/githubexample/ui/editWork/EditWorkFragment.java
@@ -0,0 +1,73 @@
+package com.laioffer.githubexample.ui.editWork;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.laioffer.githubexample.base.BaseFragment;
+import com.laioffer.githubexample.databinding.EditWorkFragmentBinding;
+import com.laioffer.githubexample.ui.NavigationManager;
+import com.laioffer.githubexample.ui.userInfo.UserInfoFragment;
+
+public class EditWorkFragment extends BaseFragment<EditWorkViewModel, EditWorkRepository> {
+    private NavigationManager navigationManager;
+    private EditWorkViewModel mViewModel;
+    private EditWorkFragmentBinding binding;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        navigationManager = (NavigationManager) context;
+    }
+
+
+    public static EditWorkFragment newInstance() {
+        return new EditWorkFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = EditWorkFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        binding.back.setOnClickListener(v -> {
+            navigationManager.navigateTo(new UserInfoFragment());
+        });
+        // TODO: Use the ViewModel
+    }
+
+    @Override
+    protected EditWorkViewModel getViewModel() {
+        return new ViewModelProvider(requireActivity(), getFactory()).get(EditWorkViewModel.class);
+    }
+
+    @Override
+    protected ViewModelProvider.Factory getFactory() {
+        return new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new EditWorkViewModel(getRepository());
+            }
+        };
+    }
+
+    @Override
+    protected EditWorkRepository getRepository() {
+        return new EditWorkRepository();
+    }
+}
