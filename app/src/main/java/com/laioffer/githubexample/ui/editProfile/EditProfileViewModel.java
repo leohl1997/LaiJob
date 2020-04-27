692830f98550e8d48e8c815200f23d3c99a10497
leo <leih5900@gmail.com>
2020-04-26 21:43:54 -0500

    Create EditProfileViewModel.java

---
+++ app/src/main/java/com/laioffer/githubexample/ui/editProfile/EditProfileViewModel.java
@@ -0,0 +1,10 @@
+package com.laioffer.githubexample.ui.editProfile;
+
+import com.laioffer.githubexample.base.BaseViewModel;
+
+public class EditProfileViewModel extends BaseViewModel<EditProfileRepository> {
+    public EditProfileViewModel(EditProfileRepository baseRepository) {
+        super(baseRepository);
+    }
+    // TODO: Implement the ViewModel
+}
