f962ebabb29d2e4ef5d45d6bed51c449a60a7176
leo <leih5900@gmail.com>
2020-04-26 21:52:46 -0500

    Update Config.java

--- app/src/main/java/com/laioffer/githubexample/util/Config.java
+++ app/src/main/java/com/laioffer/githubexample/util/Config.java
@@ -1,10 +1,23 @@
 package com.laioffer.githubexample.util;
+<<<<<<< HEAD
 
 import android.graphics.Bitmap;
 
 public class config {
+=======
+import android.content.Context;
+import android.location.LocationManager;
+public class Config {
+>>>>>>> bc0c1af1071fcf9a78b627c46fc9c842d2395ee1
 
     public static String username = "leo";
-    public static double lat = 0.0;
-    public static double lon = 0.0;
+
+
+    public static String userId;
+    public static String name;
+    public static double latitude;
+    public static double longitude;
+
+
+
 }
