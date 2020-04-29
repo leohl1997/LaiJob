f4c08c4b83855260db5f832ccee3193742258de3
leo <leih5900@gmail.com>
2020-04-29 00:20:36 -0500

    Update EditWorkEvent.java

--- app/src/main/java/com/laioffer/githubexample/ui/editWork/EditWorkEvent.java
+++ app/src/main/java/com/laioffer/githubexample/ui/editWork/EditWorkEvent.java
@@ -1,14 +1,52 @@
 package com.laioffer.githubexample.ui.editWork;
 
 import com.google.gson.annotations.SerializedName;
+import com.laioffer.githubexample.util.Config;
 
 public class EditWorkEvent {
+    @SerializedName("user_id")
+    public String userId;
+    @SerializedName("schoolName")
+    public String schoolName;
+    @SerializedName("schoolStartData")
+    public String startDate;
+    @SerializedName("schoolEndData")
+    public String endDate;
+    @SerializedName("firstName")
+    public String FirstName;
+    @SerializedName("lastName")
+    public String LastName;
+    @SerializedName("phone")
+    public String PhoneNumber;
+    @SerializedName("email")
+    public String Email;
+    @SerializedName("address")
+    public String Address;
+    @SerializedName("dataOfBirth")
+    public String DateOfBirth;
     @SerializedName("company")
-    public static String CompanyName;
+    public String CompanyName;
     @SerializedName("job")
-    public static String JobTitle;
+    public String JobTitle;
     @SerializedName("jobStartData")
-    public static String StartDate;
+    public String jobStartDate;
     @SerializedName("jobEndData")
-    public static String EndDate;
+    public String jobEndDate;
+
+    EditWorkEvent(String companyName, String jobTitle, String jobStartDate,String jobEndDate){
+        this.userId = Config.userId;
+        this.schoolName = Config.schoolName;
+        this.startDate = Config.schoolStartData;
+        this.endDate = Config.schoolEndData;
+        this.Address = Config.address;
+        this.DateOfBirth = Config.dataOfBirth;
+        this.Email = Config.email;
+        this.FirstName = Config.firstName;
+        this.LastName = Config.lastName;
+        this.JobTitle = jobTitle;
+        this.jobEndDate = jobEndDate;
+        this.jobStartDate = jobStartDate;
+        this.CompanyName = companyName;
+        this.PhoneNumber = Config.phone;
+    }
 }
