d2466c8f5f94687d11fc4c78edfb6b10d0eb3d6f
leo <leih5900@gmail.com>
2020-04-29 00:20:42 -0500

    Update EditWorkViewModel.java

--- app/src/main/java/com/laioffer/githubexample/ui/editWork/EditWorkViewModel.java
+++ app/src/main/java/com/laioffer/githubexample/ui/editWork/EditWorkViewModel.java
@@ -1,10 +1,42 @@
 package com.laioffer.githubexample.ui.editWork;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+
 import com.laioffer.githubexample.base.BaseViewModel;
+import com.laioffer.githubexample.remote.response.RemoteResponse;
 
 public class editWorkViewModel extends BaseViewModel<EditWorkRepository> {
     protected editWorkViewModel(EditWorkRepository baseRepository) {
         super(baseRepository);
     }
     // TODO: Implement the ViewModel
+    private MutableLiveData<EditWorkEvent> editProfileEventMutableLiveData = new MutableLiveData<>();
+    private LiveData<RemoteResponse<EditWorkEvent>> responseLiveData = Transformations
+            .switchMap(editProfileEventMutableLiveData, repository::editWork);
+    private MutableLiveData<String> msgMutableLiveData = new MutableLiveData<>();
+
+
+    // TODO: Implement the ViewModel
+
+    public void sendWork(EditWorkEvent editWorkEvent){
+        if (editWorkEvent.FirstName == null) {
+            msgMutableLiveData.postValue("School name can not be empty!");
+            return;
+        }
+        if (editWorkEvent.LastName == null) {
+            msgMutableLiveData.postValue("School name can not be empty!");
+            return;
+        }
+        editProfileEventMutableLiveData.postValue(editWorkEvent);
+    }
+    public LiveData<RemoteResponse<EditWorkEvent>> getResponseLiveData() {
+        return responseLiveData;
+    }
+
+    public MutableLiveData<String> getMsgMutableLiveData() {
+        return msgMutableLiveData;
+    }
+
 }
