af2efec17c8281222dddfd30f7e83d5ea16608f5
leo <leih5900@gmail.com>
2020-04-29 03:00:49 -0500

    Update EditProfileFragment.java

--- app/src/main/java/com/laioffer/githubexample/ui/editProfile/EditProfileFragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/editProfile/EditProfileFragment.java
@@ -3,27 +3,39 @@ package com.laioffer.githubexample.ui.editProfile;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import android.text.InputType;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.laioffer.githubexample.base.BaseFragment;
 import com.laioffer.githubexample.databinding.EditProfileFragmentBinding;
 import com.laioffer.githubexample.ui.NavigationManager;
 import com.laioffer.githubexample.ui.userInfo.UserInfoFragment;
 import com.laioffer.githubexample.util.Config;
+import com.laioffer.githubexample.util.DateUtil;
 import com.laioffer.githubexample.util.Utils;
+
+import java.util.Objects;
+
 
 public class EditProfileFragment extends BaseFragment<EditProfileViewModel, EditProfileRepository> {
     private NavigationManager navigationManager;
     private EditProfileViewModel mViewModel;
     private EditProfileFragmentBinding binding;
+    //final Calendar calendar = Calendar.getInstance();
 
     @Override
     public void onAttach(Context context) {
@@ -41,7 +53,12 @@ public class EditProfileFragment extends BaseFragment<EditProfileViewModel, Edit
                              @Nullable Bundle savedInstanceState) {
         binding = EditProfileFragmentBinding.inflate(inflater, container, false);
         binding.back.setOnClickListener(v -> {
-            navigationManager.navigateTo(new UserInfoFragment());
+            navigationManager.navigateWithFragmentDestroy(new UserInfoFragment(),EditProfileFragment.newInstance());
+        });
+        TextInputEditText textInputEditText = binding.dateBirth;
+        textInputEditText.setInputType(InputType.TYPE_NUMBER_VARIATION_NORMAL);
+        textInputEditText.setOnClickListener(v -> {
+            onCalenderClicked();
         });
         binding.save.setOnClickListener(v -> {
             Config.firstName = binding.firstName.getText().toString();
@@ -49,7 +66,6 @@ public class EditProfileFragment extends BaseFragment<EditProfileViewModel, Edit
             Config.phone = binding.phoneNumber.getText().toString();
             Config.email = binding.email.getText().toString();
             Config.address = binding.address.getText().toString();
-            Config.dataOfBirth = binding.dateBirth.getText().toString();
             viewModel.sendProfile(new EditProfileEvent(binding.firstName.getText().toString(),
                     binding.lastName.getText().toString(),
                     binding.phoneNumber.getText().toString(),
@@ -97,5 +113,26 @@ public class EditProfileFragment extends BaseFragment<EditProfileViewModel, Edit
     @Override
     protected EditProfileRepository getRepository() {
         return new EditProfileRepository();
+    }
+
+    private void onCalenderClicked(){
+        final Calendar calendar = Calendar.getInstance();
+        binding.dateBirth.setText(DateUtil.date2String(calendar.getTime(),DateUtil.YMD_FORMAT));
+        DatePickerDialog dialog = new DatePickerDialog(Objects.requireNonNull(getContext()), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                LogUtils.d(TAG, "onDateSet: year: " + year + ", month: " + month + ", dayOfMonth: " + dayOfMonth);
+
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                Config.dataOfBirth = DateUtil.date2String(calendar.getTime(),DateUtil.YMD_FORMAT);
+                binding.dateBirth.setText(DateUtil.date2String(calendar.getTime(),DateUtil.YMD_FORMAT));
+            }
+        },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH) );
+        dialog.show();
     }
 }
