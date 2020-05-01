a1890cfd9f5ea30c55495763fb6a7056a4cef402
leo <leih5900@gmail.com>
2020-05-01 15:42:46 -0500

    Create data.java

---
+++ app/src/main/java/com/laioffer/githubexample/ui/search/data.java
@@ -0,0 +1,10 @@
+package com.laioffer.githubexample.ui.search;
+
+public class data {
+
+    public static String [] data = {"Java","kotlin","C","C++","C#","Python","PHP","JavaScript"};
+
+    public static String get(int position) {
+        return data[position];
+    }
+}
