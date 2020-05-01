24f63818e111b3eadfde40153b2666c17067f22d
leo <leih5900@gmail.com>
2020-05-01 15:42:50 -0500

    Update SearchFragment.java

--- app/src/main/java/com/laioffer/githubexample/ui/search/SearchFragment.java
+++ app/src/main/java/com/laioffer/githubexample/ui/search/SearchFragment.java
@@ -1,21 +1,25 @@
 package com.laioffer.githubexample.ui.search;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.laioffer.githubexample.R;
@@ -28,53 +32,41 @@ import com.laioffer.githubexample.util.Utils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class SearchFragment extends BaseFragment<SearchViewModel, SearchRepository> implements AdapterView.OnItemSelectedListener  {
     SearchEvent searchEvent = new SearchEvent(0,"");
-
     private SearchViewModel mViewModel;
     EditText t1;
-
+    private ListView mListView;
     public static SearchFragment newInstance() {
         return new SearchFragment();
     }
     private NavigationManager navigationManager;
-
+    private SearchFragmentBinding binding;
     public void setFilterRule(int filterRule) {
         this.searchEvent.setFilterRule(filterRule);
     }
+    //private String [] data = {"Java","kotlin","C","C++","C#","Python","PHP","JavaScript"};
+    //
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         navigationManager = (NavigationManager) context;
     }
 
+    @SuppressLint("ResourceType")
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
-        Spinner spinner = view.findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getContext(), R.array.numbers, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
-
 
     }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if (position == 0 || position == 1){
-            this.searchEvent.setFilterRule(position);
-        }
-        else if (position == 2){
-            this.searchEvent.setFilterRule(3);
-        }
-        else {
-            this.searchEvent.setFilterRule(7);
-        }
-        //String text = parent.getItemAtPosition(position).toString();
-        //Toast.makeText(parent.getContext(), Integer.toString(filterRule), Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -83,34 +75,54 @@ public class SearchFragment extends BaseFragment<SearchViewModel, SearchReposito
     }
 
 
+    @SuppressLint("ResourceType")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
 
-        View view = inflater.inflate(R.layout.search_fragment, container, false);
-        t1 = (EditText) view.findViewById(R.id.search_view) ;
+       binding = SearchFragmentBinding.inflate(inflater, container, false);
 
-        t1.setOnClickListener(new View.OnClickListener() {
+        ItemAdapter mAdapter = new ItemAdapter(Objects.requireNonNull(this.getActivity()),R.layout.list_item, Arrays.asList(data.data));
+        binding.listview.setAdapter(mAdapter);
+        binding.listview.setTextFilterEnabled(true);
+        binding.searchView.setSubmitButtonEnabled(true);
+        binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View v) {
-            }
-        });
-
-        Button button1 = view.findViewById(R.id.search_button);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            public boolean onQueryTextSubmit(String query) {
                 if (searchEvent == null){
                     searchEvent = new SearchEvent(0,"");
                 }
                 // Utils.constructToast(getContext(),searchEvent.getKeyWord()).show();
-                searchEvent.setKeyWord(t1.getText().toString());
+                EditText mEdit = (SearchView.SearchAutoComplete)binding.searchView.findViewById(R.id.search_src_text);
+                searchEvent.setKeyWord(mEdit.getText().toString());
                 navigationManager.navigateTo(new HomeListFragment(searchEvent));
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (!TextUtils.isEmpty(newText)) {
+//              mListView.setFilterText(newText);
+                    mAdapter.getFilter().filter(newText);
+                } else {
+                    binding.listview.clearTextFilter();
+                }
+                return true;
+            }
+        });
+        binding.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = data.get(position);
+//                //HashMap<String,String> map=(HashMap<String,String>)binding.listview.getItemAtPosition(position);
+                EditText mEdit = (SearchView.SearchAutoComplete)binding.searchView.findViewById(R.id.search_src_text);
+                mEdit.setText(item);
+                binding.searchView.setSubmitButtonEnabled(true);
             }
         });
 
-        return view;
+        return binding.getRoot();
     }
 
 
